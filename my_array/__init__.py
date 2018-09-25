e6e1fc124fade6e98f81de11189a794a4e3a5bf0
Ramanathan Meyyappan <ramnathrms_2k@yahoo.com>
2018-09-25 12:48:46 -0700

    stats Push

--- my_array/__init__.py
+++ my_array/__init__.py
@@ -1,5 +1,7 @@
 # init
 
+import statistics
+
 ram_constant = 999
 
 def square(x):
@@ -12,3 +14,15 @@ class Array:
 		
 	def sum(self):
 		return sum(self.data)
+		
+	def min(self):
+		return min(self.data)
+		
+	def max(self):
+		return max(self.data)
+		
+	def mean(self):
+		return statistics.mean(self.data)
+		
+	def median(self):
+		return statistics.median(self.data)

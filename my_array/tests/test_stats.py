a1f8a13efe4c6d817f1395edfb005949213feb22
Ramanathan Meyyappan <ramnathrms_2k@yahoo.com>
2018-09-25 13:54:52 -0700

    pytest_test_code Push

--- my_array/tests/test_stats.py
+++ my_array/tests/test_stats.py
@@ -1,7 +1,19 @@
 from .. import Array
+import statistics
 
 arr = Array([2, 3, 5, 4, 7, -8, 9, 10, -7])
 
 def test_sum():
     assert arr.sum() == 25
-    assert arr.sum() == 27
+
+def test_min():
+    assert arr.min() == -8
+
+def test_max():
+    assert arr.max() == 10
+
+def test_mean():
+    assert arr.mean() == statistics.mean(arr.data)
+
+def test_median():
+    assert arr.median() == statistics.median(arr.data)
